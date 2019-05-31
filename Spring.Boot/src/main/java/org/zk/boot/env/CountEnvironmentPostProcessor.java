7e8593f6545cb5f3a33e55b213451dca638e45ad
zuokai666 <13703391897@163.com>
2019-05-31 17:47:18 +0800

    完成boot环境初始化的分析

---
+++ Spring.Boot/src/main/java/org/zk/boot/env/CountEnvironmentPostProcessor.java
@@ -0,0 +1,32 @@
+package org.zk.boot.env;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.Ordered;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
+
+/**
+ * 查看注入的属性源
+ * 
+ * @author King
+ * 
+ */
+public class CountEnvironmentPostProcessor implements EnvironmentPostProcessor, Ordered {
+	
+	private int order = 0;//已经是最大了
+	
+	@Override
+	public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+		MutablePropertySources propertySources = environment.getPropertySources();
+		for(PropertySource<?> propertySource:propertySources){
+			System.err.println(propertySource.toString());
+		}
+	}
+	
+	@Override
+	public int getOrder() {
+		return order;
+	}
+}
--- Spring.Boot/src/test/java/org/zk/core/factory/load/SpringFactoriesLoaderTest.java
+++ Spring.Boot/src/test/java/org/zk/core/factory/load/SpringFactoriesLoaderTest.java
@@ -2,6 +2,8 @@ package org.zk.core.factory.load;
 
 import java.util.List;
 
+import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.boot.env.PropertySourceLoader;
 import org.springframework.core.io.support.SpringFactoriesLoader;
 
@@ -9,6 +11,13 @@ public class SpringFactoriesLoaderTest {
 	
 	public static void main(String[] args) {
 		Class<?> factoryClass = PropertySourceLoader.class;
+		
+		factoryClass = SpringApplicationRunListener.class;
+		
+		factoryClass = EnvironmentPostProcessor.class;
+		
+		factoryClass = PropertySourceLoader.class;
+		
 		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 		List<String> result = SpringFactoriesLoader.loadFactoryNames(factoryClass, classLoader);
 		for(String con : result){
